f591df41b9c009a9f454ede90add7a7c23f23fb2
Daniel Clark <danclarkcodes@gmail.com>
2026-08-04 20:06:36 +0100

    feat: install curl in bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,7 @@ set -euo pipefail
 REPO_DIR="$HOME/code/github.com/danclark-codes/dotfiles"
 
 sudo apt update
-sudo apt install -y git
+sudo apt install -y git curl
 
 mkdir -p "$(dirname "$REPO_DIR")"
 [ -d "$REPO_DIR" ] || git clone https://github.com/danclark-codes/dotfiles "$REPO_DIR"
